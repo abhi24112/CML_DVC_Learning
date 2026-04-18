f02337958f3ecc1d65825d65205725368faaa956
abhi24112 <onlystudy24112002@gmail.com>
2026-04-18 11:52:33 +0530

    Fix drop columns axis parameter in main function

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -101,7 +101,7 @@ def main():
 
     # Impute and scale features
     weather_features_processed = impute_and_scale_data(
-        weather.drop(columns=TARGET_COLUMN, axis=1)
+        weather.drop(columns=TARGET_COLUMN)
     )
 
     # Write processed dataset
